75bd1777718c5edc2d59b1d3c735c839e92c2845
Buezrello <igindin@gmail.com>
2019-02-16 15:28:22 +0200

    docstring in readfile package

---
+++ readfile/__init__.py
@@ -0,0 +1,11 @@
+"""
+Package used for reading text file and stored lines into list
+
+Files
+-----
+fileread.py : contains Fileread class
+
+Classes
+-------
+Fileread : class that does all work
+"""
--- readfile/fileread.py
+++ readfile/fileread.py
@@ -2,14 +2,56 @@ import os
 
 
 class Fileread:
+    """
+    A class used for file reading
+
+    Attributes
+    ----------
+        filename : str
+            file with absolute or relative path
+        shortfilename : str
+            file name without path
+        lines : list
+            list of lines from a file
+
+    Methods
+    -------
+        __init__(filepath)
+            Initialization a class with absolute or relative filename
+        read_filelines()
+            Inner method that read text from file
+    """
 
     def __init__(self, filepath):
+        """
+        :param filepath: str
+            file with absolute or relative path
+
+        Attributes
+        ----------
+        filename : str
+            file with absolute or relative path
+        shortfilename : str
+            file name without path
+        lines : list
+            list of lines from a file
+        """
+
         head, tail = os.path.split(filepath)
         self.filename = filepath
         self.shortfilename = tail
-        self.lines = self._read_filelines()
+        self.lines = self.read_filelines()
 
-    def _read_filelines(self):
+    def read_filelines(self):
+        """
+
+        :return: list lines of given file
+
+        Raises
+        ------
+            Exception on failure file reading
+        """
+
         try:
             with open(self.filename, "r") as infile:
                 return infile.readlines()
